5390a1baa0efe77e850035dc8dcdaa2c289e82bb
Juan <juang15x@gmail.com>
2022-08-08 13:26:12 -0500

    Adding Splash

---
+++ app/src/main/java/com/juanguicj/inventa_tu_tienda/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.juanguicj.inventa_tu_tienda.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juanguicj.inventa_tu_tienda.R
+import com.juanguicj.inventa_tu_tienda.main.MainActivity
+import java.util.*
+import kotlin.concurrent.timer
+import kotlin.concurrent.timerTask
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val timer = Timer()
+
+        timer.schedule(
+            timerTask {
+                intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        ,2000
+        )
+    }
+}
